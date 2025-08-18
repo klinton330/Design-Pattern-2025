486531d23ba4f13e04eea222424098c33abe53c6
klinton <klinton@PTLLTP075.POINTELINDIA.COM>
2025-08-19 01:51:02 +0530

    Deleted Singleton

--- Singleton_DP1/src/com/singleton/Eager.java
+++ Singleton_DP1/src/com/singleton/Eager.java
@@ -1,8 +1,7 @@
 package com.singleton;
 
-
 public class Eager {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+       System.out.println("hello");
+	}
 }
